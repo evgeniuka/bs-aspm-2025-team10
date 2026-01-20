9c4678523a34fbbdf522af4423b52ab96bde0f1d
Ewgeniuka <oklahoman35@gmail.com>
2026-01-20 13:38:06 +0200

    fix start

--- backend/app.py
+++ backend/app.py
@@ -226,3 +226,7 @@ def register_socket_handlers():
 #     if __name__ == "__main__":
 #         allow = os.getenv("ALLOW_UNSAFE_WERKZEUG", "1") == "1"
 #         socketio.run(app, host="127.0.0.1", port=5000, debug=True, allow_unsafe_werkzeug=True)
+
+
+
+
